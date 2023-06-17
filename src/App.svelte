3fca6bc5f72a14831bae44f4bca8b5c6722bcee6
djccnt15 <djccnt15@gmail.com>
2023-06-17 16:40:27 +0900

    create user create

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,12 @@
   import Router from 'svelte-spa-router'
   import Home from "./routes/Home.svelte"
   import Post from "./routes/Post.svelte"
+  import UserCreate from "./routes/UserCreate.svelte"
 
   const routes = {
     '/': Home,
     '/post/:id': Post,
+    '/user-create': UserCreate,
   }
 </script>
 
