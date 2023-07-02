d2d4b212a3d39d560675e74334886ef8fa6147aa
djccnt15 <djccnt15@gmail.com>
2023-07-02 15:31:53 +0900

    create pagination in each post list page

--- src/routes/Community.svelte
+++ src/routes/Community.svelte
@@ -3,16 +3,26 @@
   import { link } from "svelte-spa-router"
 
   let list_post = []
+  let size = 10
+  let page = 0
+  let total = 0
+  $: total_page = Math.ceil(total/size)
 
 
-  function get_list_post() {
-    fastapi("get", "/api/board/list/community", {}, (json) => {
+  function get_list_post(_page) {
+    let params = {
+      page: _page,
+      size: size,
+    }
+    fastapi("get", "/api/board/list/community", params, (json) => {
       list_post = json.post_list
+      page = _page
+      total = json.total
     })
   }
 
 
-  get_list_post()
+  get_list_post(0)
 </script>
 
 <div class="container my-3">
@@ -38,4 +48,34 @@
       {/each}
     </tbody>
   </table>
+
+  <!-- pagination start -->
+  <ul class="pagination justify-content-center">
+    <!-- first page -->
+    <li class="page-item {page <= 0 && "disabled"}">
+      <button class="page-link" on:click="{() => get_list_post(0)}">처음</button>
+    </li>
+    <!-- previous page -->
+    <li class="page-item {page <= 0 && "disabled"}">
+      <button class="page-link" on:click="{() => get_list_post(page-1)}">이전</button>
+    </li>
+    <!-- page num -->
+    {#each Array(total_page) as _, loop_page}
+      {#if loop_page >= page-5 && loop_page <= page+5}
+      <li class="page-item {loop_page === page && "active"}">
+        <button on:click="{() => get_list_post(loop_page)}" class="page-link">{loop_page+1}</button>
+      </li>
+      {/if}
+    {/each}
+    <!-- next page -->
+    <li class="page-item {page >= total_page-1 && "disabled"}">
+      <button class="page-link" on:click="{() => get_list_post(page+1)}">다음</button>
+    </li>
+    <li class="page-item {page >= total_page-1 && "disabled"}">
+      <button class="page-link" on:click="{() => get_list_post(total_page-1)}">끝</button>
+    </li>
+  </ul>
+  <!-- pagination end -->
+
+  <a use:link href="/question-create" class="btn btn-primary">질문 등록하기</a>
 </div>
