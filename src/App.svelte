1fa55404620701fc51f97454f76060c2ad111697
djccnt15 <djccnt15@gmail.com>
2023-07-02 12:51:12 +0900

    create qna, community page

--- src/App.svelte
+++ src/App.svelte
@@ -3,11 +3,15 @@
   import Navigation from "./components/Navigation.svelte"
   import Home from "./routes/Home.svelte"
   import Post from "./routes/Post.svelte"
+  import Qna from "./routes/Qna.svelte"
+  import Community from "./routes/Community.svelte"
   import UserCreate from "./routes/UserCreate.svelte"
 
   const routes = {
     "/": Home,
     "/post/:id_post": Post,
+    "/qna": Qna,
+    "/community": Community,
     "/user-create": UserCreate,
   }
 </script>
---
+++ src/routes/Qna.svelte
@@ -0,0 +1,41 @@
+<script>
+  import fastapi from "../lib/api"
+  import { link } from "svelte-spa-router"
+
+  let list_post = []
+
+
+  function get_list_post() {
+    fastapi("get", "/api/board/list/qna", {}, (json) => {
+      list_post = json.post_list
+    })
+  }
+
+
+  get_list_post()
+</script>
+
+<div class="container my-3">
+  <table class="table">
+    <thead>
+      <tr class="table-dark">
+        <th>번호</th>
+        <th>제목</th>
+        <th>작성일시</th>
+        <th>작성자</th>
+      </tr>
+    </thead>
+    <tbody>
+      {#each list_post as post, i}
+        <tr>
+          <td>{i+1}</td>
+          <td>
+            <a use:link href="/post/{post.post.id}">{post.content.subject}</a>
+          </td>
+          <td>{post.post.date_create}</td>
+          <td>{post.user.username}</td>
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+</div>
